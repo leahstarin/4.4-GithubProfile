a814b1232dc22fe74fe625a62bd6fba83a91c36c
Leah Starin <leahstarin@LeahStarinsMBP.attlocal.net>
2016-02-26 22:12:37 -0500

    halfway there

---
+++ app/scripts/githubtoken.js
@@ -0,0 +1,1 @@
+module.exports = {'token': '37ee0798ebc2f1b514c7b6abdb01af85f3a3b50d'};
--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,1 +1,60 @@
-console.log("Hello World!");
+'use strict';
+var $ = require('jquery');
+var _ = require('underscore');
+var handlebars = require('handlebars');
+var baseUrl = 'https://api.github.com/users/leahstarin';
+var githubtoken = require('./githubtoken.js').token;
+var repoUrl = baseUrl + '/repos';
+//
+if(typeof(githubtoken) !== "undefined"){
+  $.ajaxSetup({
+    headers: {
+      'Authorization': 'token ' + githubtoken,
+    }
+  });
+}
+
+
+$.ajax(baseUrl).done(function(data){
+  console.log(data);
+  avatarTemplate(data);
+  // profilePic(data);
+  // fullName(data);
+  sideBar(data);
+});
+
+$.ajax(repoUrl).done(function(data){
+  console.log(data);
+  // _.each(data,function(item) {
+  //     // console.log(item.name);
+  // });
+});
+
+
+function avatarTemplate(data){
+  var source = $("#show-template").html();
+  var template = handlebars.compile(source);
+  var compiledTemplate = template(data);
+  $('.show-template').html(compiledTemplate);
+}
+//
+function sideBar(data){
+  var sourcetwo = $('#second-template').html();
+  var templatetwo = handlebars.compile(sourcetwo);
+  var compiledTemplatetwo = templatetwo(data);
+  $('.profilepic').html(compiledTemplatetwo);
+}
+
+// function profilePic (data){
+//   var source = $("#show-template").html();
+//   var template = handlebars.compile(source);
+//   var compiledTemplate = template(data);
+//   $('.profilepic').html(compiledTemplate);
+// }
+//
+// function fullName (data){
+//   var source = $("#second-template").html();
+//   var template = handlebars.compile(source);
+//   var compiledTemplate = template(data);
+//   $('.fullname').html(compiledTemplate);
+// }
