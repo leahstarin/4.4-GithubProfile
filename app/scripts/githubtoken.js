22c86bab8a3488b8b840b9e8fdf9242272f68d4d
Leah Starin <leahstarin@Leah-Starins-MacBook-Pro.local>
2016-02-29 15:02:59 -0500

    last push

--- app/scripts/githubtoken.js
+++
@@ -1,1 +0,0 @@
-module.exports = {'token': '37ee0798ebc2f1b514c7b6abdb01af85f3a3b50d'};
--- app/scripts/index.js
+++ app/scripts/index.js
@@ -3,9 +3,9 @@ var $ = require('jquery');
 var _ = require('underscore');
 var handlebars = require('handlebars');
 var baseUrl = 'https://api.github.com/users/leahstarin';
-var githubtoken = require('./githubtoken.js').token;
+var githubtoken;  
 var repoUrl = baseUrl + '/repos';
-//
+
 if(typeof(githubtoken) !== "undefined"){
   $.ajaxSetup({
     headers: {
@@ -14,22 +14,19 @@ if(typeof(githubtoken) !== "undefined"){
   });
 }
 
-
 $.ajax(baseUrl).done(function(data){
-  console.log(data);
-  avatarTemplate(data);
-  // profilePic(data);
-  // fullName(data);
-  sideBar(data);
-});
+ //console.log(data);
+ avatarTemplate(data);
+ // profilePic(data);
+ // fullName(data);
+ sideBar(data);
+})
 
 $.ajax(repoUrl).done(function(data){
-  console.log(data);
-  // _.each(data,function(item) {
-  //     // console.log(item.name);
-  // });
-});
-
+ console.log(data);
+ repoList(data);
+ //humanTime(data);
+})
 
 function avatarTemplate(data){
   var source = $("#show-template").html();
@@ -37,24 +34,16 @@ function avatarTemplate(data){
   var compiledTemplate = template(data);
   $('.show-template').html(compiledTemplate);
 }
-//
+
 function sideBar(data){
   var sourcetwo = $('#second-template').html();
   var templatetwo = handlebars.compile(sourcetwo);
   var compiledTemplatetwo = templatetwo(data);
   $('.profilepic').html(compiledTemplatetwo);
 }
-
-// function profilePic (data){
-//   var source = $("#show-template").html();
-//   var template = handlebars.compile(source);
-//   var compiledTemplate = template(data);
-//   $('.profilepic').html(compiledTemplate);
-// }
-//
-// function fullName (data){
-//   var source = $("#second-template").html();
-//   var template = handlebars.compile(source);
-//   var compiledTemplate = template(data);
-//   $('.fullname').html(compiledTemplate);
-// }
+function repoList(data){
+var sourcethree = $("#third-template").html();
+var templatethree = handlebars.compile(sourcethree);
+var compiledTemplatethree = templatethree({'repo': data});
+$('.repos-list').html(compiledTemplatethree);
+}
